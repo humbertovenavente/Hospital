93ec1d8327a39c8b3148a055e72c1f6283a32123
Jose Hospital <jose@hospital.com>
2025-08-10 23:24:33 -0600

    fix: Resolver conflicto de nombres 'config' en HistoriaView.vue

--- src/views/HistoriaView.vue
+++ src/views/HistoriaView.vue
@@ -56,7 +56,7 @@ const historia = ref(null);
 const timelineData = ref([]);
 const meritosData = ref([]);
 
-const config = {
+const axiosConfig = {
   headers: {
     "Content-Type": "application/json"
   }
@@ -64,7 +64,7 @@ const config = {
 
 onMounted(async () => {
   try {
-    const response = await axios.get(`${API_URL}/historias/publicadas`, config);
+    const response = await axios.get(`${config.API_URL}/historias/publicadas`, axiosConfig);
     const data = response.data;
 
     if (Array.isArray(data) && data.length > 0) {
