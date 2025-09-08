639a7bdcdc4ba613df23f847dac34d2493205772
Jose Hospital <jose@hospital.com>
2025-09-08 00:14:40 -0600

    Cambiar header a Hospital QA

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ const myAccountRoute = () => {
     <div class="navbar-container">
       <div class="brand">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
-        <h1>Dev Environment Hospital</h1>
+        <h1>Hospital QA</h1>
       </div>
 
       <nav class="nav-links">
