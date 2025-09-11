7fb814cf91fab4e5c0afca837ac9ffa2fba70fa1
Jose Hospital <jose@hospital.com>
2025-09-11 17:37:49 -0600

    Resolve merge conflict: update title to DEver 530 11/09

--- src/App.vue
+++ src/App.vue
@@ -36,11 +36,7 @@ const myAccountRoute = () => {
     <div class="navbar-container">
       <div class="brand">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
-<<<<<<< HEAD
-        <h1>Magic 14-07-2025-UAT</h1>
-=======
-        <h1>Dev Environment Hospital</h1>
->>>>>>> 648dadd5258233ca3ac4f2574bd67d8933f60087
+        <h1>DEver 530 11/09</h1>
       </div>
 
       <nav class="nav-links">
